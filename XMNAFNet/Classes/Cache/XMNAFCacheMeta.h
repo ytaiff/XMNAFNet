559dc681edb7a8b585253dbd8274775ee5366e8e
XMFraker <3057600441@qq.com>
2018-05-21 10:12:26 +0800

    新增cacheMeta类，用于记录缓存相关信息

---
+++ XMNAFNet/Classes/Cache/XMNAFCacheMeta.h
@@ -0,0 +1,39 @@
+//
+//  XMNAFCacheObject.h
+//  XMNAFNetworkDemo
+//
+//  Created by XMFraker on 16/4/22.
+//  Copyright © 2016年 XMFraker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class XMNAFNetworkRequest;
+@interface XMNAFCacheMeta : NSObject
+
+/** request 缓存版本 */
+@property (copy, nonatomic)   NSString *cachedVersion;
+/** request 缓存过期时间 */
+@property (copy, nonatomic)   NSDate *expiredDate;
+/** request 缓存的具体数据 */
+@property (copy, nonatomic)   NSData *cachedData;
+/** request 缓存的response.headers */
+@property (copy, nonatomic)   NSDictionary *cachedResponseHeaders;
+/** request 缓存数据是否已经过期 */
+@property (assign, nonatomic, readonly) BOOL isExpired;
+
+
+/**
+ 初始化方法, 根据request生成对应的CacheMeta实例
+ 保存对应的返回数据,过期时间,缓存版本等信息
+ @param request 已经请求完成的request实例
+ @return XMNAFCacheMeta实例
+ */
+- (instancetype)initWithRequest:(__kindof XMNAFNetworkRequest *)request;
++ (instancetype)cacheMetaWithRequest:(__kindof XMNAFNetworkRequest *)request;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XMNAFNet/Classes/Cache/XMNAFCacheMeta.m
@@ -0,0 +1,49 @@
+//
+//  XMNAFCacheObject.m
+//  XMNAFNetworkDemo
+//
+//  Created by XMFraker on 16/4/22.
+//  Copyright © 2016年 XMFraker. All rights reserved.
+//
+
+#import "XMNAFCacheMeta.h"
+#import "XMNAFNetworkRequest.h"
+
+#import <YYModel/YYModel.h>
+
+@interface XMNAFCacheMeta ()
+
+@end
+
+@implementation XMNAFCacheMeta
+
+#pragma mark - Life Cycle
+
++ (instancetype)cacheMetaWithRequest:(__kindof XMNAFNetworkRequest *)request {
+    return [[XMNAFCacheMeta alloc] initWithRequest:request];
+}
+
+- (instancetype)initWithRequest:(__kindof XMNAFNetworkRequest *)request {
+    if (!request) return nil;
+    if (self = [super init]) {
+        _cachedData = request.response.responseData;
+        _expiredDate = [NSDate dateWithTimeIntervalSinceNow:request.cacheTime];
+    }
+    return self;
+}
+
+#pragma mark - Override
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    return [self yy_modelEncodeWithCoder:aCoder];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    return [self yy_modelInitWithCoder:aDecoder];
+}
+
+#pragma mark - Getter
+
+- (BOOL)isExpired { return [self.expiredDate timeIntervalSinceDate:[NSDate date]] <= 0; }
+
+@end
